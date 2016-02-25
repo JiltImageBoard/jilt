f1caa17a3e94dcb94796808583f918c7af3495af
insciq <redshadow613@gmail.com>
2016-02-25 18:07:27 +0300

    DataFormatter phpdoc fix

--- common/helpers/DataFormatter.php
+++ common/helpers/DataFormatter.php
@@ -6,7 +6,7 @@ class DataFormatter
 {
     /**
      * @param array $objects
-     * @return \stdClass
+     * @return array
      */
     public static function mergeFieldsToObj($objects)
     {
@@ -45,9 +45,14 @@ class DataFormatter
         return $obj;
     }
 
-    public static function camelCaseToUnderscore($input)
+    /**
+     * Converting string from camel case to underscore
+     * @param string $str
+     * @return string
+     */
+    public static function camelCaseToUnderscore($str)
     {
-        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
+        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $str, $matches);
         $ret = $matches[0];
         foreach ($ret as &$match) {
             $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
