75aab2949756829fcf60e28fd72d522a6463a438
insciq <redshadow613@gmail.com>
2016-03-11 00:28:36 +0300

    minor syntax fix

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -3,7 +3,6 @@
 namespace app\controllers;
 
 use app\models\Thread;
-=======
 use app\common\helpers\ArrayHelper;
 use app\common\helpers\DataFormatter;
 use app\common\classes\MultiLoader;
@@ -11,10 +10,10 @@ use app\common\classes\Cleanup;
 
 use app\models\Board;
 use app\models\BoardCounter;
-use app\models\Thread;
 use app\models\Post;
 use app\models\PostData;
 use app\models\PostMessage;
+use yii\web\Controller;
 
 class TestController extends Controller
 {
